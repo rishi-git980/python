7fa6e998ac9cb43d56632fd472d35de34e81b403
Nalike Rishi <rishinalike@gmail.com>
2026-09-01 10:56:09 +0530

    Implement Rock-Paper-Scissors game logic
    Added functions to get player and computer choices, and to check the winner in a Rock-Paper-Scissors game.

--- RPS/RPS.py
+++ RPS/RPS.py
@@ -1,1 +1,33 @@
+import random
+def get_choices():
+ player_choice = input("Enter a choice (rock, paper, scissors):")
+ computer_choice = random.choice(["rock", "paper", "scissors"])
+ choices = {"player": player_choice, "computer": computer_choice}
+ return choices
 
+def check_win(player,computer):
+ print(f"You chose {player}, computer chose {computer}.")
+ if player == computer:
+  return "It's a tie!"
+ 
+ elif player == "rock":
+  if computer == "scissors":
+   return "Rock smashes scissors! player wins!"
+  else:
+   return "paper covers rock! computer wins!"
+
+ elif player == "paper":
+  if computer == "rock":
+   return "Paper covers rock! player wins!"
+  else:
+   return "Scissors cut paper! computer wins!"
+ 
+ elif player == "scissors":
+  if computer == "paper":
+   return "Scissors cut paper! player wins!"
+  else:
+   return "Rock smashes scissors! computer wins!" 
+
+choices=get_choices()
+result=check_win(choices["player"],choices["computer"])
+print(result)
